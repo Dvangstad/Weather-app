299b71386c6591e1db8cae69570072b7060cc9f5
Derek Vangstad <vang0519@yahoo.com>
2021-05-26 16:14:16 -0500

    another commit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,29 @@
 let weather = {
     "apiKey": "09a1324c114a018c52a1f76bb8ab90e0",
-    fetchWeather: function() {
+    fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=standard&appid=09a1324c114a018c52a1f76bb8ab90e0"
-        ).then
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp = "°C";
+        document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed:" + speed + "m/h";
+
+
     }
-}
+};
