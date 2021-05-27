78f3a5bee17fe956674268bef82f687c4939442f
Derek Vangstad <vang0519@yahoo.com>
2021-05-26 19:42:49 -0500

    Final commit

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,6 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity)
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -23,7 +22,29 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed:" + speed + "mi/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
 
+
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
 
     }
 };
+
+document.querySelector(".search button")
+.addEventListener("click", function () {
+    weather.search();
+
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+
+    }
+
+});
+
+weather.fetchWeather("Denver");
